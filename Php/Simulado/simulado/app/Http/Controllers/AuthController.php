a18e2a990894d942d76cc95b38b1d8a69b934a2b
IgorOliverx <igorferoli019@gmail.com>
2023-11-16 11:39:55 -0300

    sdffvd

--- Php/Simulado/simulado/app/Http/Controllers/AuthController.php
+++ Php/Simulado/simulado/app/Http/Controllers/AuthController.php
@@ -2,25 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+
+    //esse metodo é necessário para a requisição via GET
+    public function mostrarLogin()
     {
-        $request->validate([
-            'username' => 'required|string',
-            'password' => 'required|string',
+        //so retorna a view quando o user acessa o endpoint
+        return view('login');
+    }
+
+    //metodo principal de login
+    public function login(Request $request)//Recebe uma requisição no construtor (POST)
+    {
+        $request->session()->reflash();// é utilizada em conjunto com o return back
+
+        $credenciais = $request->only('username', 'password');
+
+        $request->merge([
+            'username' => $credenciais['username'],
+            'password' => $credenciais['password'],
         ]);
 
-        $credentials = $request->only('username', 'password');
+        $passaAcesso = User::query()->get('accessToken');//passando acesso da consulta ao bd
+        $teste = Auth::user()->accessToken;//isso aqui faz com que pegue os dados somente do usuario em questão
 
-        if (Auth::attempt($credentials)) {
-            $token = auth()->user()->createToken('API Token')->accessToken;
-            return response()->json(['token' => $token], 200);
+        if (Auth::attempt($credenciais)) {
+            return redirect()->intended("/?$teste");
         }
 
-        return response()->json(['error' => 'Credenciais inválidas'], 401);
+        return back()->withErrors("Credenciais inválidas");
     }
+
 
 }
